3128852554a207d4f03c199a9c273f909c7411d3
DUrizarSis <daurizar5@gmail.com>
2024-03-01 00:24:24 -0600

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,9 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(()=> console.log("Connected to MongoDB Atlas"))
+    .catch((error)=> console.error(error))
+
+app.listen(PORT, () => {
     console.log("Server is running on", PORT )
 });
